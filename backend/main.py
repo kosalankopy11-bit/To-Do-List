6a0a5af54f7a03890d53834e79b9f3a43b5ffb88
K.Kosalan <kosalankopy11@gmail.com>
2026-07-07 07:00:34 +0530

    saved my local work before pull

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.database import Base, engine
 from app.routes import task_route
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Todo List App")
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",  
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=origins,       
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(task_route.router)
-
 
 @app.get("/")
 def read_root():
