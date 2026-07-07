d108358fb45767373f328375c324b093d987577d
thulasikauthayakumar <thulasikathulasi625@gmail.com>
2026-07-07 09:56:40 +0530

    update

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,13 +1,1 @@
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-
-class Settings:
-   DATABASE_URL: str = os.getenv("DATABASE_URL")
-   APP_NAME: str = os.getenv("APP_NAME", "Todo List App")
-
-
-settings = Settings()
+DATABASE_URL = "sqlite:///./todo.db"
